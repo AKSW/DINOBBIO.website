8b844bfccbf2befaf3161103d277b8dca3ba2923
Paulo do Carmo <31288239+PauloRVdC@users.noreply.github.com>
2023-06-30 11:57:31 +0200

    safari iframe test

--- assets/widgets/iframe_external.js
+++
@@ -1,5 +0,0 @@
-var external_link = window.location.hash.substring(1);
-var external_hostname = window.location.hostname;
-var external_p = document.getElementById('external-link');
-
-external_p.innerHTML=external_p.innerHTML.replace('<link>',external_link).replace('<link>',external_link.split('?')[0]);
